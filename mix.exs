ca65a9870fc4822213a302680d8d259eaa66c3a0
Kevin Schweikert <kevin@kevinschweikert.de>
2024-10-20 20:52:47 +0200

    update telemetry_metrics to stable 1.0

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Membrane.TelemetryMetrics.Mixfile do
   defp deps do
     [
       {:telemetry, "~> 1.0"},
-      {:telemetry_metrics, "~> 0.6.1"},
+      {:telemetry_metrics, "~> 1.0"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, ">= 0.0.0", only: :dev, runtime: false}
